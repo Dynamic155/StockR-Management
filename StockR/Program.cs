d609e1f18390447c1206c9556b007694d2280543
Dynamic <79714443+Dynamic155@users.noreply.github.com>
2024-05-29 08:42:49 +0200

    Added defualt C# template.

--- StockR/Program.cs
+++ StockR/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StockR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To StockR");
+        }
+    }
+}
